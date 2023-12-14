c5a5adfa5b426590d6fbe7648df3f69bcf7722c5
YF <chooiyaofeng@gmail.com>
2023-12-14 16:27:50 +0800

    part 1 done

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const Button =({handleClick,text})=>(
+  <button onClick={handleClick}>{text}</button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  
+  const initialVotes = new Array(anecdotes.length).fill(0);
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(initialVotes);
+
+  const handleClick = ()=>{
+    setSelected(Math.floor(Math.random() * 7))
+  }
+  const handleVote = () => {
+    // Create a copy of the votes array and increment the selected anecdote's vote count
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  }
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>Has {votes[selected]} votes</p>
+      <br />
+      <Button handleClick={handleVote} text="vote"/>
+      <Button handleClick={handleClick} text="next anecdote"/>
+      <br />
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+      <br />
+      <p>Has {Math.max(...votes)} votes</p>
+    </div>
+    
+  )
+}
+
+export default App
